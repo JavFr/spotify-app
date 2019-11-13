862ab4c94c0a77f001473dc1c5f9a141eaf5b3fb
JavFr <jfbrustolon@gmail.com>
2019-11-13 15:55:23 -0300

    change redirectUri

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export const authEndpoint = "https://accounts.spotify.com/authorize";
 
 export const clientId = "9464d0a757e94b8184997bb23fc228a1";
-export const redirectUri = "http://localhost:3000/";
+export const redirectUri = "https://javfr.github.io/spotify-app/index";
 export const scopes = [
   "user-read-currently-playing",
   "user-read-playback-state",
